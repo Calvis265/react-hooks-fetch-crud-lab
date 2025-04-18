accb7c7b0513020c3187a4fefedd1e0521a35cda
calvis onyango <calvisonyango265@gmail.com>
2025-04-18 11:50:55 +0300

    react

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,38 @@
-import React from "react";
+// import React from "react";
+
+// function QuestionList() {
+//   return (
+//     <section>
+//       <h1>Quiz Questions</h1>
+//       <ul>{/* display QuestionItem components here after fetching */}</ul>
+//     </section>
+//   );
+// }
+
+// export default QuestionList;
+
+import React, { useState, useEffect } from "react";
+import QuestionItem from "./QuestionItem"; 
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);  
+
+   
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")  
+      .then((response) => response.json()) 
+      .then((data) => setQuestions(data))  
+      .catch((error) => console.error("Error fetching questions:", error));  
+  }, []);  
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questions.map((question) => (
+          <QuestionItem key={question.id} question={question} />  
+        ))}
+      </ul>
     </section>
   );
 }
